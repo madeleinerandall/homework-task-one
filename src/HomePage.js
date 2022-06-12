e79017b19743c63071433c5008cbf67f32064dde
Madeleine Daphne Randall <madeleinedaphnerandall@HUAWEI_P30_Pro-22ffad5f34.localdomain>
2022-06-12 10:31:27 +0100

    Creation of global gutters class and some animation on HomePage

--- src/HomePage.js
+++ src/HomePage.js
@@ -2,9 +2,12 @@ import "./HomePage.scss";
 
 function HomePage() {
   return (
-    <main>
+    <main className="gutters">
       <div>
         <img src="../heart.png" alt="heart" />
+      </div>
+      <div>
+        <h1>I love Richard</h1>
       </div>
     </main>
   );
