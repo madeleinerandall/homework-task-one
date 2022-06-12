e9d44d1993badd4ff8ed6f69e30e6faba33e4e7f
Madeleine Daphne Randall <madeleinedaphnerandall@Madeleines-MacBook-Air.local>
2022-06-12 15:44:30 +0100

    Creation of footer

--- src/Footer.js
+++ src/Footer.js
@@ -3,9 +3,31 @@ import "./Footer.scss";
 function Footer() {
   return (
     <footer>
-      <div>
-        <p>Copyright Madeleine Randall 2022</p>
-      </div>
+      <section className="footer gutters">
+        <div id="left">
+          <h4>
+            Madeleine Randall <span>-</span>
+          </h4>
+          <h4>Profolio Website 2: a demonstration of coding skills</h4>
+          <h4>Code for the future ~</h4>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique
+            nostrum non ex fuga repudiandae vitae, quaerat obcaecati rem
+            corporis qui quidem quae saepe, dolorem ullam veritatis blanditiis!
+            Quos, alias expedita.
+          </p>
+          <nav></nav>
+        </div>
+        <div id="right">
+          <h4>right hand side (for now)</h4>
+        </div>
+      </section>
+      <section className="copyright gutters">
+        <div id="copyright">
+          <p>Copyright Madeleine Randall 2022</p>
+        </div>
+      </section>
     </footer>
   );
 }
